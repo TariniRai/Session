7c64ae2c5599bc5dec46651d47e9b040def7f9ce
TariniRai <232972@students.aih.nsw.edu.au>
2024-05-27 19:28:17 +1000

    Update session.py

--- session.py
+++ session.py
@@ -40,3 +40,17 @@ gst_pie = 0.08*pie
  print("gst_apple")
  print("gst_pie")
              
+# write a program to calculate the number of minis in the maxis
+# total maxis is 2.5*minis
+# 1 minis is 5000
+# calculate how much is 4.5 minis
+
+value for minis is 5000
+maxis is 2.5*minis
+ so 4.5 maxis is equal to 2.5*4.5*minis
+display maxis
+
+minis = 5000
+maxis = 2.5*minis
+print("the value of maxis is:",4.5*maxis)
+

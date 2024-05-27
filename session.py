eee49e5bad123b1b7552cb6d7cbcf35884dbccd1
TariniRai <232972@students.aih.nsw.edu.au>
2024-05-27 18:37:27 +1000

    Update session.py

--- session.py
+++ session.py
@@ -1,1 +1,42 @@
+#First Program
+# Calculate Area of a Circle
+# A=πr2
+#Area of circle = pie*radius**2
 
+#pie = 3.1415
+#diam = 14
+#radi = diam/2
+
+#input pie value
+#input radius vzlue
+#calculate area by:
+#pir times r square
+#display area of circle
+
+#input miles value
+#input name of the user
+#convert kms to miles by:
+#miles is 1.6*kms
+#display kms with names
+
+# write a program to input age of a user and let them know if they have a dibetese risk
+# risk or not. Use case is that a user with are more than 65 is risky else not
+# risky 
+age = 55
+if age >= 65 
+ print("This is Risky")
+else 
+print ("not risky")
+
+# write a program to calculate the GST tax of two products, apple and pie
+# apple has 10% GST and pie has only 8%
+# calculate total GST that need to be dedicated from the bill
+# apple gross sales is $75, pie gross sale is $100
+
+apple = 75
+pie = 100
+gst_apple = 0.1*apple
+gst_pie = 0.08*pie
+ print("gst_apple")
+ print("gst_pie")
+             
